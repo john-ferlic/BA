f6658460ec25f3c1403028959f52238ed1eb90bf
645566 <645566@LTUS000491.cts.com>
2017-09-21 14:30:48 -0700

    applicant details

--- src/com/cg/bankaccount/Applicant.java
+++ src/com/cg/bankaccount/Applicant.java
@@ -2,8 +2,69 @@ package com.cg.bankaccount;
 
 public class Applicant {
 	
-	public static void main(String[] args) {
-		System.out.println("hello world");
+	private int applNumber;
+	private String firstName;
+	private String midName;
+	private String lastName;
+	private String addrline1;
+	private String addrline2;
+	private String city;
+	private int zipcode;
+	private String state;
+	
+	public int getApplNumber() {
+		return applNumber;
+	}
+	public void setApplNumber(int applNumber) {
+		this.applNumber = applNumber;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMidName() {
+		return midName;
+	}
+	public void setMidName(String midName) {
+		this.midName = midName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddrline1() {
+		return addrline1;
+	}
+	public void setAddrline1(String addrline1) {
+		this.addrline1 = addrline1;
+	}
+	public String getAddrline2() {
+		return addrline2;
+	}
+	public void setAddrline2(String addrline2) {
+		this.addrline2 = addrline2;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public int getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
 	}
 
 }
